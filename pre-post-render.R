1c6f873721fc41cf37169cdf4f181f0dee7a5dd7
Kenji Hayashi <kenjithayashi@gmail.com>
2024-03-15 14:07:40 -0700

    Add markdown syntax for images

--- pre-post-render.R
+++ pre-post-render.R
@@ -1,4 +1,4 @@
-# Clean up copied images dir to maintain reprex
+# Clean up copied images dir in qmd dir to maintain reprex
 if (dir.exists("qmd/images")) {
   unlink("qmd/images", recursive = TRUE)
 }
